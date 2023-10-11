c0dd3a00633a0372405a746499310f5465ca7356
Jack Krause <jmkrause@iastate.edu>
2023-10-10 21:24:08 -0500

    onclick loads some json data

---
+++ project/index.js
@@ -0,0 +1,45 @@
+const jsonUrl = "./data.json";
+
+function getMovie(movieId) {
+    const movieElement = document.getElementById('c1');
+
+    fetch(jsonUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("${response.status}");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const comedies = data.genres.comedyMovies;
+            const movie = comedies.find(movie => movie.id === movieId);
+
+            if (movie) {
+                movieElement.innerHTML = `
+                <h2>${movie.title}</h2>
+                <p>Starring: ${movie.star}</p>
+                <p>IMDb Rating: ${movie.imdbRating}</p>
+                <p>Genre: ${movie.genre}</p>
+                <p>URL: <a href="${movie.url}">IMDb Link</a></p>
+            `;
+            } else {
+                movieElement.innerHTML = 'Movie not found';
+            }
+        })
+        .catch(error => {
+            console.error('Error', error);
+        });
+}
+
+//show more info for comedy-c1
+const c1Button = document.getElementById("c1_button");
+
+c1Button.addEventListener("click", function() {
+    getMovie('c1');
+});
+
+
+
+
+
+
